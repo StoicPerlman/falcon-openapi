91f5bccfedb805564027dab51ad848b094b59c5e
Sam Kleiner <sam@skleiner.com>
2018-06-03 12:30:10 -0400

    add more setup settings

--- setup.py
+++ setup.py
@@ -3,11 +3,14 @@ from setuptools import setup
 
 setup(
     name='Falcon OpenApi',
+    python_requires='>3.5.0',
     version='0.1.0',
     description='Falcon router to map openapi spec to resources',
     author='Sam Kleiner',
     author_email='sam@skleiner.com',
     url='https://github.com/StoicPerlman/falcon-openapi/',
+    download_url = 'https://github.com/StoicPerlman/falcon-openapi/archive/0.1.0.tar.gz',
+    keywords = ['falcon', 'openapi', 'api'],
     packages=['falcon_openapi'],
     install_requires=[
         'falcon',
