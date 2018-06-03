869f71dbf6f720c976b8aa0c971278b729023dc4
Sam Kleiner <sam@skleiner.com>
2018-06-03 13:20:45 -0400

    convert readme to rst for pypi

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
+import pypandoc
 from setuptools import setup
 
-with open('README.md') as f:
-    readme = f.read()
+readme = pypandoc.convert('README.md', 'rst')
 
 with open('LICENSE') as f:
     license = f.read()
@@ -10,7 +10,7 @@ with open('LICENSE') as f:
 setup(
     name='falcon-openapi',
     python_requires='>3.5.0',
-    version='0.1.3',
+    version='0.1.4',
     description='Falcon router to map openapi spec to resources',
     long_description=readme,
     long_description_content_type="text/markdown",
@@ -18,7 +18,7 @@ setup(
     author_email='sam@skleiner.com',
     license=license,
     url='https://github.com/StoicPerlman/falcon-openapi/',
-    download_url = 'https://github.com/StoicPerlman/falcon-openapi/archive/0.1.3.tar.gz',
+    download_url = 'https://github.com/StoicPerlman/falcon-openapi/archive/0.1.4.tar.gz',
     keywords = ['falcon', 'openapi', 'api'],
     packages=['falcon_openapi'],
     install_requires=[
