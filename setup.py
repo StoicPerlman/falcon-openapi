a944915a0e6166b32898b2245abd9dde0611c0a4
Sam Kleiner <sam@skleiner.com>
2018-06-03 13:26:17 -0400

    rst readme again. why is pypi like this

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python
-import pypandoc
+import os
 from setuptools import setup
 
-readme = pypandoc.convert('README.md', 'rst')
+readme_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'README.md')
+try:
+    from m2r import parse_from_file
+    readme = parse_from_file(readme_file)
+except ImportError:
+    # m2r may not be installed in user environment
+    with open(readme_file) as f:
+        readme = f.read()
 
 with open('LICENSE') as f:
     license = f.read()
@@ -10,7 +17,7 @@ with open('LICENSE') as f:
 setup(
     name='falcon-openapi',
     python_requires='>3.5.0',
-    version='0.1.4',
+    version='0.1.5',
     description='Falcon router to map openapi spec to resources',
     long_description=readme,
     long_description_content_type="text/markdown",
@@ -18,7 +25,7 @@ setup(
     author_email='sam@skleiner.com',
     license=license,
     url='https://github.com/StoicPerlman/falcon-openapi/',
-    download_url = 'https://github.com/StoicPerlman/falcon-openapi/archive/0.1.4.tar.gz',
+    download_url = 'https://github.com/StoicPerlman/falcon-openapi/archive/0.1.5.tar.gz',
     keywords = ['falcon', 'openapi', 'api'],
     packages=['falcon_openapi'],
     install_requires=[
