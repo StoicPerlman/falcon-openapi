48f42d24eb3da43bd3665ccb876d21a9a9da3425
Sam Kleiner <sam@skleiner.com>
2018-06-03 13:47:02 -0400

    remove licence from setup

--- setup.py
+++ setup.py
@@ -4,21 +4,18 @@ from setuptools import setup
 with open('README.md') as f:
     readme = f.read()
 
-with open('LICENSE') as f:
-    license = f.read()
-
 setup(
     name='falcon-openapi',
     python_requires='>3.5.0',
-    version='0.1.6',
+    version='0.1.7',
     description='Falcon router to map openapi spec to resources',
     long_description=readme,
     long_description_content_type="text/markdown",
     author='Sam Kleiner',
     author_email='sam@skleiner.com',
-    license=license,
+    license='MIT',
     url='https://github.com/StoicPerlman/falcon-openapi/',
-    download_url = 'https://github.com/StoicPerlman/falcon-openapi/archive/0.1.6.tar.gz',
+    download_url = 'https://github.com/StoicPerlman/falcon-openapi/archive/0.1.7.tar.gz',
     keywords = ['falcon', 'openapi', 'api'],
     packages=['falcon_openapi'],
     install_requires=[
