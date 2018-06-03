b1b3e3d73d79fcb3def2f09435e5bf59b018c055
Sam Kleiner <sam@skleiner.com>
2018-06-03 12:53:39 -0400

    add readme and license to description

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python
 from setuptools import setup
 
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
 setup(
     name='falcon-openapi',
     python_requires='>3.5.0',
     version='0.1.0',
     description='Falcon router to map openapi spec to resources',
+    long_description=readme,
     author='Sam Kleiner',
     author_email='sam@skleiner.com',
+    license=license,
     url='https://github.com/StoicPerlman/falcon-openapi/',
     download_url = 'https://github.com/StoicPerlman/falcon-openapi/archive/0.1.0.tar.gz',
     keywords = ['falcon', 'openapi', 'api'],
