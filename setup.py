d27211dc47d3bf1c38b7306ae2cb0dd1a236ec4a
Sam Kleiner <sam@skleiner.com>
2018-06-03 13:34:46 -0400

    markdown it is

--- setup.py
+++ setup.py
@@ -1,15 +1,8 @@
 #!/usr/bin/env python
-import os
 from setuptools import setup
 
-readme_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'README.md')
-try:
-    from m2r import parse_from_file
-    readme = parse_from_file(readme_file)
-except ImportError:
-    # m2r may not be installed in user environment
-    with open(readme_file) as f:
-        readme = f.read()
+with open('README.md') as f:
+    readme = f.read()
 
 with open('LICENSE') as f:
     license = f.read()
@@ -17,7 +10,7 @@ with open('LICENSE') as f:
 setup(
     name='falcon-openapi',
     python_requires='>3.5.0',
-    version='0.1.5',
+    version='0.1.6',
     description='Falcon router to map openapi spec to resources',
     long_description=readme,
     long_description_content_type="text/markdown",
@@ -25,7 +18,7 @@ setup(
     author_email='sam@skleiner.com',
     license=license,
     url='https://github.com/StoicPerlman/falcon-openapi/',
-    download_url = 'https://github.com/StoicPerlman/falcon-openapi/archive/0.1.5.tar.gz',
+    download_url = 'https://github.com/StoicPerlman/falcon-openapi/archive/0.1.6.tar.gz',
     keywords = ['falcon', 'openapi', 'api'],
     packages=['falcon_openapi'],
     install_requires=[
